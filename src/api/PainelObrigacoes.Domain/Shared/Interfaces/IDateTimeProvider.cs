ba2d8088de3251ea67f8887bc3f3e2e4b171689d
Gabriel Massaia <gabrielmassaia50@gmail.com>
2026-06-09 10:59:32 -0300

    feat(domain): add IDateTimeProvider abstraction for testable time access

---
+++ src/api/PainelObrigacoes.Domain/Shared/Interfaces/IDateTimeProvider.cs
@@ -0,0 +1,8 @@
+namespace PainelObrigacoes.Domain.Shared.Interfaces;
+
+public interface IDateTimeProvider
+{
+    DateTime UtcNow { get; }
+    int CurrentYear { get; }
+    int CurrentMonth { get; }
+}
